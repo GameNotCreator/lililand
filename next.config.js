990c9c011bb529c49515399279c0bdc7e915cbd9
GameNotCreator <78528763+GameNotCreator@users.noreply.github.com>
2024-01-08 14:10:31 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         domains: ['instagram.ftun1-2.fna.fbcdn.net', 'i.imgur.com'],
     },
     env: {
-        LOGO: 'https://instagram.ftun1-2.fna.fbcdn.net/v/t51.2885-19/412664904_1419594472096088_5756466967687574181_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.ftun1-2.fna.fbcdn.net&_nc_cat=108&_nc_ohc=3AtysSEnnIwAX8MP5G1&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfBCulCcU12Lw8Vq-XYmEa_BG2QmnnhyXLEfC_z5TtpuZQ&oe=659856A7&_nc_sid=8b3546',
+        LOGO: 'https://i.imgur.com/572vqhd.jpg',
     }
 }
 
