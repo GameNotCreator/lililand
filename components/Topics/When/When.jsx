4dd6834d5b48719e1fbc3a0eec1e0a9d46c0bdea
GameNotCreator <gnc.ytb@gmail.com>
2024-01-28 13:49:10 -0500

    v1

--- components/Topics/When/When.jsx
+++ components/Topics/When/When.jsx
@@ -12,7 +12,7 @@ const When = () => {
         </div>
         <center>
         <iframe
-          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%234285F4&ctz=Europe%2FParis&showTitle=0&showPrint=0&showTabs=0&showTz=0&showCalendars=0&src=ZTI1YzVlZTIwYTg0ZmZiNTFiYWIwZTk2MDY4YTQ1OTUwMDVmMzBmZDg2NjJjOGQ5YTc2OTUyMTU4NmEwNzJiNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00"          style={{ borderWidth: 0 }}
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%234285F4&ctz=Europe%2FParis&showTitle=0&showPrint=0&showTabs=0&showTz=0&showCalendars=0&src=ZTI1YzVlZTIwYTg0ZmZiNTFiYWIwZTk2MDY4YTQ1OTUwMDVmMzBmZDg2NjJjOGQ5YTc2OTUyMTU4NmEwNzJiNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00&hl=fr"          style={{ borderWidth: 0 }}
           width="800"
           height="600"
           className='sm:flex hidden'
@@ -22,7 +22,7 @@ const When = () => {
         ></iframe>
         <br /><br />  
         <iframe
-          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%234285F4&ctz=Europe%2FParis&showTitle=0&showPrint=0&showTabs=0&showTz=0&showCalendars=0&src=ZTI1YzVlZTIwYTg0ZmZiNTFiYWIwZTk2MDY4YTQ1OTUwMDVmMzBmZDg2NjJjOGQ5YTc2OTUyMTU4NmEwNzJiNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00"          style={{ borderWidth: 0 }}
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%234285F4&ctz=Europe%2FParis&showTitle=0&showPrint=0&showTabs=0&showTz=0&showCalendars=0&src=ZTI1YzVlZTIwYTg0ZmZiNTFiYWIwZTk2MDY4YTQ1OTUwMDVmMzBmZDg2NjJjOGQ5YTc2OTUyMTU4NmEwNzJiNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23EF6C00&hl=fr"          style={{ borderWidth: 0 }}
           className='sm:hidden w-full h-96'
           frameborder="0"
           lang="fr"
