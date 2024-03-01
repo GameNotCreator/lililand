6d9ac481fa1af9569b1740af54530388163a41cb
GameNotCreator <gnc.ytb@gmail.com>
2024-03-01 11:15:53 -0500

    add pwa + add metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,8 +3,28 @@ import Footer from '@components/Layout/Footer/Footer'
 import Navbar from '@components/Layout/Navbar/Navbar'
 
 export const metadata = {
+  manifest: '/manifest.json',
   title: 'Lili Land La Marsa',
-  description: 'Parc d\'attractions pour enfants à La Marsa',
+  description: 'Parc d\'attractions Lili Land à La Marsa',
+  creator: "H&H corp",
+  openGraph: {
+    type: "website",
+    locale: "fr_FR",
+    url: 'https://lililand.tn' ,
+    title: 'Lili Land La Marsa',
+    description: 'Parc d\'attractions pour enfants à La Marsa',
+    siteName: 'Lili Land La Marsa',
+    images: [
+      {
+        url: '/logofooter',
+        width: 1200,
+        height: 630,
+        alt: 'Image du parc Lili Land La Marsa',
+      },
+    ],
+  },
+
+
 }
 
 export default function RootLayout({ children }) {
--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+    dest: "public",
+    // cacheOnFrontEndNav: true,
+    // aggressiveFrontEndNavCaching: true,
+    // reloadOnOnline: true,
+    // swcMinify: true,
+    // disable: false,
+    // workboxOptions: {
+    //   disableDevLogs: true,
+    // },
+  });
+
 const nextConfig = {
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
